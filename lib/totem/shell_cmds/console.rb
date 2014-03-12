bb98fabf5ba53c0a126f0d49dc3db7dcfca04430
Chad Remesch <chad@remesch.com>
2014-03-12 14:44:37 -0400

    Auto load environment.rb in shell console.

--- lib/totem/shell_cmds/console.rb
+++ lib/totem/shell_cmds/console.rb
@@ -4,6 +4,14 @@ module Totem
   module ShellCmds
     class Console < Totem::ShellCmds::Base
       def run
+        env_path = 'config/environment.rb'
+        if File.exists?(env_path)
+          load(env_path)
+        else
+          puts "Unable to find #{env_path}.  You must run this command from your project root directory."
+          exit
+        end
+
         ARGV.clear
         IRB.start
       end
